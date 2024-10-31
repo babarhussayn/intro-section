f3b59965447ff472150e566437e3759ac7691d76
babarhussain <babarhusain262@gmail.com>
2024-10-31 15:50:00 +0500

    complete

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -27,6 +27,10 @@ const simpleLink = [
 ];
 
 export function Header() {
+  const [isShow, setIsShow] = React.useState(false);
+  const handleOpen = () => {
+    setIsShow((prev) => !prev);
+  };
   return (
     <>
       <div className="w-full ">
@@ -40,7 +44,7 @@ export function Header() {
                 height={50}
               />
             </div>
-            <div className="">
+            <div className="hidden md:block">
               <NavigationMenu>
                 <NavigationMenuList>
                   <NavigationMenuItem>
@@ -139,7 +143,7 @@ export function Header() {
               </NavigationMenu>
             </div>
           </div>
-          <div className="w-full">
+          <div className="w-full hidden md:block">
             <div className="flex justify-end items-center gap-8">
               <div>
                 <Button className="bg-white text-[#adadad] hover:bg-white">
@@ -152,6 +156,145 @@ export function Header() {
                 </Button>
               </div>
             </div>
+          </div>
+          {/********************************Mobile screens ********************************/}
+
+          <div className="md:hidden top-0 ">
+            {isShow ? (
+              <div className="flex justify-center items-center flex-col right-0">
+                <div>
+                  <Image
+                    src="/images/icon-close-menu.svg"
+                    alt="menu-icon"
+                    width={50}
+                    height={50}
+                    onClick={handleOpen}
+                  />
+                </div>
+                <div className="flex justify-center items-center flex-col absolute top-0 mt-16 bg-white w-[70%] h-full right-0 transition-all duration-100 ease-in ">
+                  <div className=" md:hidden flex justify-center items-center flex-col">
+                    <NavigationMenu className="flex justify-center items-center flex-col">
+                      <NavigationMenuList className="flex justify-center items-center flex-col gap-8">
+                        <NavigationMenuItem>
+                          <NavigationMenuTrigger className="text-[#adadad] font-bold">
+                            Features
+                          </NavigationMenuTrigger>
+                          <NavigationMenuContent>
+                            <ul className="grid gap-3 p-6 w-[150px] justify-center items-center ">
+                              <li className="flex justify-center items-center gap-4">
+                                <div className="w-full">
+                                  <Image
+                                    src="/images/icon-todo.svg"
+                                    alt="todo"
+                                    width={20}
+                                    height={30}
+                                  />{" "}
+                                </div>
+                                <div className="flex justify-end w-full">
+                                  <h4 className="text-center">Todo</h4>
+                                </div>
+                              </li>
+                              <li className="flex justify-center items-center gap-4">
+                                {" "}
+                                <div className="w-full">
+                                  <Image
+                                    src="/images/icon-calendar.svg"
+                                    alt="todo"
+                                    width={20}
+                                    height={30}
+                                  />{" "}
+                                </div>
+                                <div className="flex w-full">
+                                  <h4>Calendar</h4>
+                                </div>
+                              </li>
+                              <li className="flex justify-center items-center gap-4">
+                                {" "}
+                                <div className="w-full">
+                                  <Image
+                                    src="/images/icon-reminders.svg"
+                                    alt="todo"
+                                    width={20}
+                                    height={30}
+                                  />
+                                </div>
+                                <div>
+                                  <p> Reminders</p>
+                                </div>
+                              </li>
+                              <li className="flex justify-center items-center gap-4">
+                                {" "}
+                                <div className="w-full">
+                                  <Image
+                                    src="/images/icon-planning.svg"
+                                    alt="todo"
+                                    width={20}
+                                    height={30}
+                                  />
+                                </div>
+                                <div>
+                                  <p> Planing</p>
+                                </div>
+                              </li>
+                            </ul>
+                          </NavigationMenuContent>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                          <NavigationMenuTrigger className="text-[#adadad] font-bold">
+                            Company
+                          </NavigationMenuTrigger>
+                          <NavigationMenuContent>
+                            <ul className="grid gap-3 p-6 w-[150px]">
+                              <li>History</li>
+                              <li>Our Team</li>
+                              <li>Blog</li>
+                            </ul>
+                          </NavigationMenuContent>
+                        </NavigationMenuItem>
+                        {simpleLink.map((item, index) => (
+                          <NavigationMenuItem key={index}>
+                            <div className="flex justify-center items-center ">
+                              <Link href={item.link} legacyBehavior passHref>
+                                <NavigationMenuLink
+                                  className={navigationMenuTriggerStyle()}
+                                >
+                                  <h3 className="text-[#adadad] font-bold">
+                                    {" "}
+                                    {item.name}
+                                  </h3>
+                                </NavigationMenuLink>
+                              </Link>
+                            </div>
+                          </NavigationMenuItem>
+                        ))}
+                      </NavigationMenuList>
+                    </NavigationMenu>
+                  </div>
+                  <div className="w-full  md:hidden flex justify-center items-center flex-col">
+                    <div className="flex justify-end items-center flex-col gap-8">
+                      <div>
+                        <Button className="bg-white text-[#adadad] hover:bg-white">
+                          Login
+                        </Button>
+                      </div>
+                      <div>
+                        <Button className="bg-white text-[#adadad] border border-black hover:text-white ">
+                          Register
+                        </Button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <Image
+                src="/images/icon-menu.svg"
+                alt="close"
+                width={50}
+                height={50}
+                onClick={handleOpen}
+              />
+            )}
           </div>
         </div>
       </div>
--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -31,31 +31,44 @@ const HomePage = () => {
     <div className="w-full flex justify-center items-center  mt-8">
       <div className=" w-[1400px]">
         <div className="flex justify-center items-center ">
-          <div className="grid grid-cols-2 gap-28 w-full">
+          <div className="md:grid md:grid-cols-2 md:gap-28 gap-6 w-full flex-col flex justify-center items-center">
+            <div className="w-full flex justify-center items-center md:hidden">
+              {" "}
+              <Image
+                src="/images/image-hero-mobile.png"
+                alt="mobile"
+                height={200}
+                width={700}
+              />
+            </div>
             <div className=" flex justify-center items-center">
               <div className="w-full flex justify-center flex-col gap-8">
                 <div className="">
-                  <div className="">
-                    <h3 className="text-7xl text-start font-bold">Make </h3>
-                    <p className="text-7xl text-start font-bold">remote work</p>
+                  <div className="flex justify-center items-center gap-4 md:block">
+                    <h3 className="md:text-7xl text-4xl text-start font-bold">
+                      Make{" "}
+                    </h3>
+                    <p className="md:text-7xl text-4xl text-start font-bold">
+                      remote work
+                    </p>
                   </div>
                 </div>
-                <div className="flex justify-start items-center w-full">
-                  <div className="w-[400px] flex justify-end items-center">
-                    <p className="text-lg text-start text-[#adadad]">
+                <div className="flex md:justify-start justify-center items-center w-full">
+                  <div className="md:w-[400px] w-[350px] flex md:justify-end justify-center items-center">
+                    <p className="text-lg md:text-start text-center text-[#adadad]">
                       Get your team in sync ,no matter your location. Streamline
                       processes, create team rituals, and watch productivity
                       soar.
                     </p>
                   </div>
                 </div>
-                <div>
+                <div className="flex justify-center items-center md:block">
                   <Button className="hover:bg-white hover:text-black">
                     Learn more
                   </Button>
                 </div>
-                <div className="flex justify-center items-center gap-8 mt-16 flex-row">
-                  <div className="flex justify-center items-center gap-8">
+                <div className="flex justify-center items-center gap-8 md:mt-16 flex-row ">
+                  <div className="flex justify-center items-center md:gap-8 gap-4 p-8 md:p-0">
                     {imageslogo.map((items, index) => (
                       <Image
                         src={items.src}
@@ -69,7 +82,7 @@ const HomePage = () => {
                 </div>
               </div>
             </div>
-            <div className="w-full flex justify-center items-center">
+            <div className="w-full hidden md:block">
               <div className="w-full flex justify-center items-center">
                 <Image
                   src="/images/image-hero-desktop.png"
